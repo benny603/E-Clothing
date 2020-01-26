a620e071c7a4fb6732185d0e67dfd56718d997c6
iambennywu <bennywu603@gmail.com>
2020-01-27 00:32:34 +0800

    adding redux create the main root-reducer, create a middleware store.js to connect with user.action to user.reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,34 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 import Iconmon from "../../assets/iconmon.png";
-import "./header.styles.scss";
 import { auth } from "../../firebase/firebase.utils";
 
-const Header = ({ currentUser }) => {
-  return (
-    <div className="header">
-      <Link className="logo-container" to="/">
-        <img src={Iconmon} alt="logo" />
-      </Link>
-      <div className="options">
-        <Link className="option" to="/shop">
-          SHOP
-        </Link>
-        <Link className="option" to="/shop">
-          CONTACT
-        </Link>
-        {currentUser ? (
-          <div className="option" onClick={() => auth.signOut()}>
-            SIGN OUT
-          </div>
-        ) : (
-          <Link className="option" to="/signin">
-            SIGN IN
-          </Link>
-        )}
-      </div>
-    </div>
-  );
-};
+import "./header.styles.scss";
 
-export default Header;
+const Header = ({ currentUser }) => (
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <img src={Iconmon} alt="logo" />
+    </Link>
+    <div className="options">
+      <Link className="option" to="/shop">
+        SHOP
+      </Link>
+      <Link className="option" to="/shop">
+        CONTACT
+      </Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className="option" to="/signin">
+          SIGN IN
+        </Link>
+      )}
+    </div>
+  </div>
+);
+
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
