51532b4c9a34a13a1f3123e5c0bfbbce9b29b77f
iambennywu <bennywu603@gmail.com>
2020-02-11 12:42:08 +0800

    adding redux-persist to prevent lose carts when refresh

---
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./collections-overview.styles.scss";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import CollectionPreview from "../collection-preview/collection-preview.component.jsx";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
+
+const collectionsOverview = ({ collections }) => (
+  <div className="collections-overview">
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollectionsForPreview
+});
+
+export default connect(mapStateToProps)(collectionsOverview);
